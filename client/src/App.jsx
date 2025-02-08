4b2961853bf26eac9266a99e7a4f591af1dd4167
Bishal1030 <bishal4shahi@gmail.com>
2025-02-08 22:30:21 +0545

    ui updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 
 const App = () => {
   const playerRef = useRef(null);
-  const [link, setLink] = useState("http://localhost:3000/uploads/courses/4672ba00-e60a-11ef-9786-05728f5ab427/index.m3u8");
+  const [link, setLink] = useState("");
 
 
   const videoLink = "http://localhost:3000/uploads/courses/4672ba00-e60a-11ef-9786-05728f5ab427/index.m3u8"
@@ -39,8 +39,13 @@ const App = () => {
 
   return (
     <>
-      <Home setLink={setLink} />
+    <h1 className='top-left'>PLAY<span className='text-customRed'>WAVE</span></h1>
+
+    <div className='transform -translate-y-20'>
+      <Home setLink={setLink} style={{ marginBottom: "4rem"}} />
       {link && <Video options={videoJsOptions} onReady={handlePlayerReady} />}
+
+    </div>
       
     </>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,19 +27,21 @@ const Home = ({ setLink }) => {
     };
 
     return (
-        <div>
-            <h1>Welcome to Play<span style={{color: "orange"}}>Wave</span></h1>
+        <div className='transform -translate-y-20'>
+            <h1 className='mb-6' style= {{fontFamily: 'Karantina, cursive'}}>Welcome to Play<span className='text-customRed'>Wave</span></h1>
+            <div className='flex flex-col items-center'>
             <input
                 type="file"
                 accept="video/*"
                 onChange={fileChangeHandler}
-            />
+                className="mb-4 mr-1 px-4 py-2 border border-gray-300 rounded-lg cursor-pointer bg-white text-gray-700 shadow-sm hover:bg-gray-100 focus:outline-none"            />
             <button 
                 onClick={handleVideo}
-                style={{marginBottom: "4rem", background: "blue"}}
+                style= {{fontFamily: 'Karantina, cursive', backgroundColor: '#B37270', color: 'white', fontSize: '1rem', fontWeight: 'bold'}}
             >
                 Upload Video
             </button>
+            </div>
         </div>
     );
 };
