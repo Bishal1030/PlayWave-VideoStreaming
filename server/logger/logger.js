c2bb763351595c0f06cb2235fcfe80437f2854b6
Bishal1030 <bishal4shahi@gmail.com>
2025-02-09 08:51:54 +0545

    ui updated

---
+++ server/logger/logger.js
@@ -0,0 +1,19 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: './logger/error.txt', level: 'error' }),
+    new winston.transports.File({ filename: 'logger/combined.txt' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+export default logger;
